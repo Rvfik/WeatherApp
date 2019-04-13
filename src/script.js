90fcff46ed1a680fddfcff0cc7de79c93d2ee3c4
Rafał Rokitowski <rokitowskirafal@gmail.com>
2019-04-13 22:44:00 +0200

    set weekday and fixed styles

--- src/script.js
+++ src/script.js
@@ -18,9 +18,6 @@ const getJSONfromAPI = async () => {
 async function getData() {
     let response = await getJSONfromAPI();
 
-    let clouds = response.clouds.all;
-    let latitude = response.coord.lat;
-    let longitude = response.coord.lon;
     let humidity = response.main.humidity;
     let pressure = response.main.pressure;
     let temp = Math.floor(response.main.temp - 273.15);
@@ -30,28 +27,19 @@ async function getData() {
     let country = response.sys.country;
     let sunrise = Unix_timestamp(response.sys.sunrise);
     let sunset = Unix_timestamp(response.sys.sunset);
-    let visibility = response.visibility;
-    let description = response.weather[0].description;
-    let parameter = response.weather[0].main;
-    let icon = response.weather[0].icon;
+    let visibility = Math.floor(response.visibility * 0.001);
     let windDeg = response.wind.deg;
     let windSpeed = response.wind.speed;
 
-    // document.getElementById('clouds').innerText = `${clouds}%`
-    // document.getElementById('latitude').innerText = `latitude: ${latitude}°`
-    // document.getElementById('longitude').innerText = `longitude: ${longitude}°`
     document.getElementById('humidity').innerText = `${humidity}%`
     document.getElementById('pressure').innerText = `${pressure} hPa`
-    document.getElementById('temp').innerText = `${temp}° C`
-    document.getElementById('tempMax').innerText = `${tempMax}° C`
-    document.getElementById('tempMin').innerText = `${tempMin}° C`
+    document.getElementById('temp').innerText = `${temp}°C`
+    document.getElementById('tempMax').innerText = `${tempMax}°C`
+    document.getElementById('tempMin').innerText = `${tempMin}°C`
     document.getElementById('cityName').innerText = `${cityName}, ${country}`
     document.getElementById('sunrise').innerText = `${sunrise}`
     document.getElementById('sunset').innerText = `${sunset}`
-    document.getElementById('visibility').innerText = `${visibility} m`
-    // document.getElementById('description').innerText = `${description}`
-    // document.getElementById('weatherParameter').innerText = `parameter: ${parameter}`
-    // document.getElementById('icon').innerText = `icon: ${icon}`
+    document.getElementById('visibility').innerText = `${visibility} km`
     // document.getElementById('windDeg').innerText = `${windDeg}°`
     document.getElementById('windSpeed').innerText = `${windSpeed} m/s`
 
@@ -91,23 +79,30 @@ async function getForecast() {
 
     let tempForecast = tempFiveDays.map(el => ({
         temp: el.main.temp,
-        icon: el.weather.map(item => item.icon).reduce(item => item)
+        icon: el.weather.map(item => item.icon).reduce(item => item),
+        weekDay: el.dt_txt
     }));
 
-    
     for (let day of tempForecast) {
 
         let indexOf = tempForecast.indexOf(day);
         let dayTemp = Math.floor(day.temp - 273.15);
+        let weekDay = new Date(day.weekDay);
         let dayIcon = day.icon;
+
+        let NumberOfWeekday = weekDay.getDay();
+        const NameOfWeekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
         let selectIconDay = `<img src=http://openweathermap.org/img/w/${dayIcon}.png>`;
 
-        document.getElementById(`temperatureDay${indexOf + 1}`).innerText = `${dayTemp}° C`;
+        document.getElementById(`temperatureDay${indexOf + 1}`).innerText = `${dayTemp}°C`;
+        document.getElementById(`weekDay${indexOf + 1}`).innerText = NameOfWeekday[NumberOfWeekday];
         document.getElementById(`iconDay${indexOf + 1}`).innerHTML = selectIconDay;
     }
 }
 getForecast()
+
+
 
 function getAllData() {
     getData();
